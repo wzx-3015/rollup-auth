e92aa1c1fee5e648ad71d3dc803aea3c24a384d9
wzx <834529118@qq.com>
2021-10-13 10:52:40 +0800

    移除lib文件

--- packages/lib/index.js
+++
@@ -1,6 +0,0 @@
-import 'vue';
-export { d as dfsjAuthSelfPermission, p as dfsjUnifyAuth, u as useDStore } from './index-2c28bd2a.js';
-import 'axios';
-import 'element-plus';
-import 'vue-router';
-//# sourceMappingURL=index.js.map
